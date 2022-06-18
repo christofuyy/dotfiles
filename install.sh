ede957d782c1d435e9b611128291e9a1ac40275d
Christopher <cyoun@chapman.edu>
2022-06-17 22:30:01 -0700

    fix: Ensure parent directories exist before installling omf and yabai

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@
 ~/.dotfiles/.config/nvchad/install.sh 
 
 # shell
+mkdir -p ~/.config/omf
 ln -s ~/.dotfiles/.config/omf/init.fish ~/.config/omf/init.fish
 
 # window manager
+mkdir -p ~/.config/yabai
 ln -s ~/.dotfiles/.config/yabai/yabairc ~/.config/yabai/yabairc
