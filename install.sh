2ba773c20c8b9ded9d2826c20a56375a820d1db7
Christopher <cyoun@chapman.edu>
2022-06-17 22:32:23 -0700

    feat: Add kitty config installation into main script

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@
 mkdir -p ~/.config/omf
 ln -s ~/.dotfiles/.config/omf/init.fish ~/.config/omf/init.fish
 
+# terminal emulator
+mkdir -p ~/.config/kitty
+ln -s ~/.dotfiles/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+
 # window manager
 mkdir -p ~/.config/yabai
 ln -s ~/.dotfiles/.config/yabai/yabairc ~/.config/yabai/yabairc
