af1864192a456b7f387d689732e4c057192788e9
Christopher <cyoun@chapman.edu>
2022-07-29 12:18:28 -0700

    feat(nvim): Remove unnecessary null-ls diagnostics

--- .config/nvchad/plugins/config/null-ls.lua
+++ .config/nvchad/plugins/config/null-ls.lua
@@ -2,20 +2,21 @@ local null_ls = require "null-ls"
 local b = null_ls.builtins
 
 local sources = {
+  -- python
   null_ls.builtins.formatting.black,
 
+  -- Web dev
   b.formatting.prettierd.with { filetypes = { "html", "markdown", "css" } },
   b.formatting.deno_fmt,
 
+  -- Java
   b.formatting.google_java_format,
 
   -- Lua
   b.formatting.stylua,
-  b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
 
   -- Shell
   b.formatting.shfmt,
-  b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
 }
 
 local M = {}
