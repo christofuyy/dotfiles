212d6d6b57baad485ceeb5106adc1b68b8275335
Christopher <cyoun@chapman.edu>
2023-01-20 18:04:49 -0800

    feat(nvim): Add dockerls to lsp servers

--- .config/nvchad/plugins/config/lsp.lua
+++ .config/nvchad/plugins/config/lsp.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "pyright", 'clangd', 'tsserver', 'jdtls', 'hls', 'ltex'}
+local servers = { "pyright", 'clangd', 'tsserver', 'hls', 'ltex', 'dockerls'}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
