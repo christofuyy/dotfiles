3389439e3fbd4a1017b74bd9b47e0651ea563463
Christopher Y <christo1321youn@gmail.com>
2023-01-21 23:24:43 -0800

    feat(bspwm): Configure bspwm and sxhkd
    This is for linux only

--- install.sh
+++ install.sh
@@ -14,3 +14,9 @@ ln -s ~/.dotfiles/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 # window manager
 mkdir -p ~/.config/yabai
 ln -s ~/.dotfiles/.config/yabai/yabairc ~/.config/yabai/yabairc
+
+mkdir -p ~/.config/bspwm
+ln -s ~/.dotfiles/.config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+
+mkdir -p ~/.config/sxhkd
+ln -s ~/.dotfiles/.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
