4da23bae566efd771915da1ba047acc79fd3efaf
Christopher <cyoun@chapman.edu>
2023-03-10 11:19:44 -0800

    feat(nvim): Add sqlls to lsp servers

--- .config/nvchad/plugins/config/lsp.lua
+++ .config/nvchad/plugins/config/lsp.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "pyright", 'clangd', 'tsserver', 'hls', 'ltex', 'dockerls', 'rust_analyzer', 'yamlls'}
+local servers = { "pyright", 'clangd', 'tsserver', 'hls', 'ltex', 'dockerls', 'rust_analyzer', 'yamlls', 'sqlls'}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
