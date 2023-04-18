e442674a0b82d13c68993714d9c56655ff557c34
Christopher <cyoun@chapman.edu>
2023-04-18 10:46:09 -0700

    feat(nvim): Remove eslint lsp

--- .config/nvchad/config/lsp.lua
+++ .config/nvchad/config/lsp.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "pyright", 'clangd', 'tsserver', 'hls', 'ltex', 'dockerls', 'rust_analyzer', 'yamlls', 'sqlls', 'eslint', 'prismals' }
+local servers = { "pyright", 'clangd', 'tsserver', 'hls', 'ltex', 'dockerls', 'rust_analyzer', 'yamlls', 'sqlls','prismals' }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
