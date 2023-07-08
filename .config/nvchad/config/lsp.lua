2efcd61277dca09e76eac80f96130c079c1a712b
Christopher <cyoun@chapman.edu>
2023-07-08 13:02:14 -0700

    feat: Add tailwindcss to lsp

--- .config/nvchad/config/lsp.lua
+++ .config/nvchad/config/lsp.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "pyright", 'clangd', 'tsserver', 'hls', 'ltex', 'dockerls', 'rust_analyzer', 'yamlls', 'sqlls','prismals' }
+local servers = { "pyright", 'clangd', 'tsserver', 'hls', 'ltex', 'dockerls', 'rust_analyzer', 'yamlls', 'sqlls','prismals' ,'tailwindcss'}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
