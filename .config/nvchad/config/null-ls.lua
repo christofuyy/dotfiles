e5baa539de4fbfa064f5ba46a5e3ef8a80922551
Christopher <cyoun@chapman.edu>
2023-11-04 12:12:47 -0700

    chore: Format

--- .config/nvchad/config/null-ls.lua
+++ .config/nvchad/config/null-ls.lua
@@ -1,50 +1,50 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 local b = null_ls.builtins
 
 local sources = {
-  -- python
-  null_ls.builtins.formatting.black,
+	-- python
+	null_ls.builtins.formatting.black,
 
-  -- Web dev
-  b.formatting.prettierd,
+	-- Web dev
+	b.formatting.prettierd,
 
-  -- Lua
-  b.formatting.stylua,
+	-- Lua
+	b.formatting.stylua,
 
-  -- Shell
-  b.formatting.shfmt,
+	-- Shell
+	b.formatting.shfmt,
 
-  -- LaTeX
-  b.formatting.latexindent,
+	-- LaTeX
+	b.formatting.latexindent,
 
-  -- Haskell
-  b.formatting.stylish_haskell,
+	-- Haskell
+	b.formatting.stylish_haskell,
 
-  -- Rust
-  b.formatting.rustfmt,
+	-- Rust
+	b.formatting.rustfmt,
 
-  -- SQL
-  b.formatting.sql_formatter,
-  -- b.formatting.sqlfluff.with({
-  --   extra_args={"--dialect","sqlite"}
-  -- })
+	-- SQL
+	b.formatting.sql_formatter,
+	-- b.formatting.sqlfluff.with({
+	--   extra_args={"--dialect","sqlite"}
+	-- })
 
-  -- C++
-  b.formatting.clang_format.with({
-    filetypes = { "c", "cpp", "h", "hpp" },
-  }),
+	-- C++
+	b.formatting.clang_format.with({
+		filetypes = { "c", "cpp", "h", "hpp" },
+	}),
 
-  -- Java
-  b.formatting.google_java_format,
+	-- Java
+	b.formatting.google_java_format,
 }
 
 local M = {}
 
 M.setup = function()
-  null_ls.setup {
-    debug = true,
-    sources = sources,
-  }
+	null_ls.setup({
+		debug = true,
+		sources = sources,
+	})
 end
 
 return M
