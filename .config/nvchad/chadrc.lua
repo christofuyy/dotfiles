07242c3c71527da6d758841438d11339e5218a1c
Christopher <cyoun@chapman.edu>
2023-12-07 12:11:24 -0800

    feat: Change diffview colors

--- .config/nvchad/chadrc.lua
+++ .config/nvchad/chadrc.lua
@@ -7,10 +7,12 @@ M.ui = {
 		theme = "minimal",
 		separator_style = "round",
 	},
-	hl_override = {
-		DiffAdd = { bg = { "blue", -50 } },
-		DiffDelete = { bg = { "red", -50 } },
-	},
+  hl_add = {
+		DiffviewDiffAdd = { bg = { "green", -50 }, fg = "green" },
+		DiffviewDiffDelete = { bg = { "red", -50 } },
+		DiffviewDiffText = { bg = { "purple", -65 }, fg = "purple" },
+		DiffviewDiffChange = { bg = { "blue", -50 }, fg = "blue" },
+  }
 }
 
 M.mappings = require("custom.mappings")
