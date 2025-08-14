a4ad9c8fc24154d4192a4acc04fef34e08de1bfa
christofuyy <christo1321youn@gmail.com>
2025-08-14 11:49:26 -0700

    Add pacman install scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+sudo pacman -Syu
+sudo pacman -S kitty nvim fish bspwm sxhkd picom pulseaudio tmux ripgrep polybar
+
 # text editor
-~/.dotfiles/.config/nvchad/install.sh 
+# ~/.dotfiles/.config/nvchad/install.sh 
 
 # shell
 mkdir -p ~/.config/omf
@@ -12,8 +15,8 @@ mkdir -p ~/.config/kitty
 ln -s ~/.dotfiles/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 
 # window manager
-mkdir -p ~/.config/yabai
-ln -s ~/.dotfiles/.config/yabai/yabairc ~/.config/yabai/yabairc
+# mkdir -p ~/.config/yabai
+# ln -s ~/.dotfiles/.config/yabai/yabairc ~/.config/yabai/yabairc
 
 mkdir -p ~/.config/bspwm
 ln -s ~/.dotfiles/.config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
@@ -22,9 +25,9 @@ mkdir -p ~/.config/sxhkd
 ln -s ~/.dotfiles/.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
 
 # polybar
-mkdir -p ~/.config/polybar
-ln -s ~/.dotfiles/.config/polybar/config.ini ~/.config/polybar/config.ini
+# mkdir -p ~/.config/polybar
+# ln -s ~/.dotfiles/.config/polybar/config.ini ~/.config/polybar/config.ini
 
 # picom
-mkdir -p ~/.config/picom
-ln -s ~/.dotfiles/.config/picom/picom ~/.config/picom/picom
+# mkdir -p ~/.config/picom
+# ln -s ~/.dotfiles/.config/picom/picom ~/.config/picom/picom
